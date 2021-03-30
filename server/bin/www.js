831621c236ee974c5ef1ea0c8913faf7d4429e69
Fawzi Najjar <fawzinajjar@icloud.com>
2021-03-30 22:01:34 +0200

    fixed git variables

--- server/bin/www.js
+++ server/bin/www.js
@@ -60,7 +60,7 @@ function onError(error) {
     throw error;
   }
 
-  var bind = typeof port === 'string'
+  const bind = typeof port === 'string'
     ? 'Pipe ' + port
     : 'Port ' + port;
 
@@ -84,8 +84,8 @@ function onError(error) {
  */
 
 function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
+  const addr = server.address();
+  const bind = typeof addr === 'string'
     ? 'pipe ' + addr
     : 'port ' + addr.port;
   debug('Listening on ' + bind);
